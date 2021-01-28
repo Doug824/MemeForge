c21b01718deddf4c1bc99e023c44349cd29875c2
Doug Hagan <doug.hagan824@gmail.com>
2021-01-28 14:38:01 -0500

    cleaned up js

--- script.js
+++ script.js
@@ -39,35 +39,3 @@ function remove(e)  {
     e.target.parentNode.remove();
 }
 });
-
-
-
-// function start() {
-//     const memeForm = document.getElementById("memeForm");
-//     memeForm.addEventListener("submit", addMeme, false);
-// }
-// window.addEventListener("load", start, false);
-
-//   // append new meme div to flexbox using template
-// function addMeme() {
-//     e.preventDefault();
-//     const memeUrl = document.getElementById("uploaded-img").value;
-//     const topTxt = document.getElementById("text-top").value;
-//     const btmTxt = document.getElementById("text-bottom").value;
-//     let newMeme = document.createElement("div");
-//     newMeme.innerHTML = document.getElementById("memeTmplt").innerHTML;
-//     newMeme.className = "meme-wrapper";
-//     document.getElementById("meme-gallery").appendChild(newMeme);
-//     newMeme.firstElementChild.src = memeUrl; // sets img src of newMeme div
-//     newMeme.children[1].innerText = topTxt; // set val of top text input from form
-//     newMeme.children[2].innerText = btmTxt; // set val of btm text input from form
-//     newMeme.id = "meme"; // sets id of meme wrapper, can refer to delete later
-//     // clear form after submit
-//     memeForm.reset();
-// }
-
-//   // remove meme upon click
-// function removeMeme() {
-//     e.target.parentNode.remove();
-// }
-// newMeme.className = "meme-wrapper";
